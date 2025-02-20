da7aac144b4e7be0510ad03b704b501e66ccd72c
Sarah Dean <xsarah.dean@gmail.com>
2025-02-20 10:56:38 -0500

    new combined estimator

--- utils/linucb.py
+++ utils/linucb.py
@@ -54,6 +54,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from sklearn.base import BaseEstimator, ClassifierMixin
+import warnings
 
 
 class TorchLogReg(nn.Module): 
@@ -67,42 +68,59 @@ class TorchLogReg(nn.Module):
         out = nn.functional.softmax(out, dim=1) 
         return out 
 
-class MaxEntLinearModel(BaseEstimator, ClassifierMixin):
-    def __init__(self, input_dim, output_dim, lr=0.01, weight_decay=0.01, epochs=100, tol=1e-4):
+
+class CombinedLinearModel(BaseEstimator, ClassifierMixin):
+    def __init__(self, input_dim, output_dim, lr=0.01, weight_decay=0.01, epochs=1000, tol=1e-4):
         self.input_dim = input_dim
         self.output_dim = output_dim
         self.lr = lr
-        self.weight_decay = weight_decay # l2 reg
+        self.weight_decay = weight_decay # l2 reg times 2
         self.epochs = epochs
         self.model = nn.Linear(input_dim, output_dim, bias=False)
-        self.criterion = nn.CrossEntropyLoss()
+        self.criterion_log = nn.CrossEntropyLoss()
+        self.criterion_sq = nn.MSELoss()
         self.optimizer = optim.SGD(self.model.parameters(), lr=self.lr, weight_decay=self.weight_decay)
         self.coef_ = np.zeros([output_dim,input_dim])
         self.tolerance = tol
     
-    def fit(self, X, y):
-        X_tensor = torch.tensor(X, dtype=torch.float32)
-        y_tensor = torch.tensor(y, dtype=torch.long)
-        
+    def fit(self, X_log, y_log, X_sq=None, y_sq=None, verbose=False):
+        X_log_tensor = torch.tensor(X_log, dtype=torch.float32)
+        y_log_tensor = torch.tensor(y_log, dtype=torch.long)
+
+        if X_sq is not None and len(X_sq)>0:
+            X_sq_tensor = torch.tensor(X_sq, dtype=torch.float32)
+            non_nan_mask = ~np.isnan(y_sq)
+            y_sq_tensor = torch.tensor(y_sq[non_nan_mask], dtype=torch.float32)
+
         previous_loss = float('inf')
         
         for epoch in range(self.epochs):
-            outputs = self.model(X_tensor)
-            loss = self.criterion(outputs, y_tensor)
+            outputs_log = self.model(X_log_tensor)
+            loss_log = self.criterion_log(outputs_log, y_log_tensor)
+
+            loss_sq = 0
+            if X_sq is not None and len(X_sq)>0:
+                outputs_sq = self.model(X_sq_tensor)
+                loss_sq = self.criterion_sq(outputs_sq[non_nan_mask], y_sq_tensor)
+
+            loss = loss_log + loss_sq
             
             self.optimizer.zero_grad()
             loss.backward()
             self.optimizer.step()
             
             # Check for convergence
-            # if abs(previous_loss - loss.item()) < self.tolerance:
-            #     print(f'Convergence reached at epoch {epoch+1}')
-            #     break
+            if abs(previous_loss - loss.item()) < self.tolerance:
+                if verbose:
+                    print(f'Convergence reached at epoch {epoch+1}')
+                break
             
             previous_loss = loss.item()
 
-            # if (epoch + 1) % 10 == 0:
-            #     print(f'Epoch [{epoch+1}/{self.epochs}], Loss: {loss.item():.4f}')
+            if (epoch + 1) % 10 == 0 and verbose:
+                print(f'Epoch [{epoch+1}/{self.epochs}], Loss: {loss.item():.4f}')
+        if epoch == self.epochs:
+            warnings.warn("Convergence criteria were not met.", UserWarning)
         self.coef_ = self.model.weight.detach().numpy()
     
     def predict(self, X):
@@ -117,97 +135,67 @@ class MaxEntLinearModel(BaseEstimator, ClassifierMixin):
         return torch.softmax(outputs, dim=1).detach().numpy()
 
 
-class OnlineLogisticRegressionOracle:
-    def __init__(self, n_features, n_actions, learning_rate=0.1, lambda_=1.0, beta=1.0,
-                 mode="sgd"):
-        self.mode = mode
-        if self.mode == "sgd":
-            self.model = SGDClassifier(loss='log_loss', learning_rate='constant', eta0=learning_rate, alpha=lambda_, fit_intercept=False)  # Multi-class logistic regression
-        elif self.mode == "logreg":
-            self.model = LogisticRegression(C=1/lambda_, fit_intercept=False)  # Multi-class logistic regression multi_class='multinomial', 
-        elif self.mode == "torch":
-            # self.model = LogisticRegression(input_size=n_features, num_classes=n_actions) 
-            # self.optimizer = torch.optim.SGD(self.model.parameters(), lr=learning_rate)
-            self.model = MaxEntLinearModel(n_features, n_actions, lr=learning_rate, weight_decay=lambda_, epochs=100, tol=1e-4)
-        else:
-            raise Exception("invalid mode for logistic regression oracle")
+
+
+class CombinedOnlineRegressionOracle:
+    def __init__(self, n_features, n_actions, learning_rate, reg_coeff, rad_log, rad_sq=None, max_epochs=1000, tol=1e-4):
+        self.model = CombinedLinearModel(n_features, n_actions, lr=learning_rate, weight_decay=reg_coeff/2, epochs=max_epochs, tol=tol)
         self.n_actions = n_actions
         self.n_features = n_features
-        self.X_sum = np.zeros((n_features, n_features))  # Accumulated X^T X
-        self.lambda_ = lambda_
-        self.beta = beta
+        self.lambda_ = reg_coeff
+        self.X_sum = self.lambda_ * np.eye(n_features)  # Accumulated X^T X
+        self.A_sum = [self.lambda_ * np.eye(n_features) for _ in range(n_actions)]
+        self.beta_log = rad_log
+        self.beta_sq = rad_sq if rad_sq is not None else rad_log
+        self.beta = self.beta_log # todo revmove
         self.Xs = []
         self.ys = []
-        # # Initialize the model with some dummy data to set the number of classes (multi-class)
-        dummy_X = np.zeros((n_actions, n_features))
-        dummy_y = np.array([i for i in range(n_actions)])  # Dummy class label
-        if self.mode == "sgd":
-            self.model.partial_fit(dummy_X, dummy_y, classes=np.arange(n_actions))  # Initialize multi-class model
-        elif self.mode == "logreg":
-            self.model.fit(dummy_X, dummy_y)  # Initialize multi-class model
+        self.rs = []
+        self.ind_log = []
+        self.ind_sq = []
 
-    def update(self, x_t, action):
-        x_t_flat = x_t.ravel()
-        self.Xs.append(x_t_flat)
+    def update(self, context, action=None, reward=None, rewards=None):
+        self.Xs.append(context.ravel())
         self.ys.append(action)
-        # Update the logistic regression model with the new data point
-        # if self.mode == "torch":
-        #     # Define training parameters 
-        #     num_iters = 1000
-              
-        #     # Train the model 
-        #     for i in range(num_iters): 
-        #         # Forward pass 
-        #         outputs = self.model(self.Xs) 
-        #         loss = self.model.criterion(outputs, self.ys) 
-          
-        #         # Backward and optimize 
-        #         self.optimizer.zero_grad() 
-        #         loss.backward() 
-        #         self.optimizer.step() 
-        if self.mode == "torch":
-            self.model.fit(self.Xs, self.ys)
-        elif len(np.unique(self.ys)) == self.n_actions:
-            self.model.fit(self.Xs, self.ys)
-        elif self.mode == "sgd":
-            self.model.partial_fit(self.Xs, self.ys)
-        self.X_sum += np.outer(x_t_flat, x_t_flat)  # Update X^T X sum for logistic regression constraint
+        self.ind_log.append((action is not None))
+        if reward is not None:
+            assert rewards is None
+            rewards = np.nan((self.n_actions))
+            rewards[action] = reward
+        self.rs.append(rewards)
+        self.ind_sq.append((rewards is not None))
+
+        self.model.fit(np.array(self.Xs)[self.ind_log], np.array(self.ys)[self.ind_log], np.array(self.Xs)[self.ind_sq], np.array(self.ys)[self.ind_sq])
+        
+        xxT = np.outer(context.ravel(), context.ravel())
+        if action is not None:
+            self.X_sum += xxT
+        if rewards is not None:
+            for a, r in enumerate(rewards):
+                if r is not None:
+                    self.A_sum[a] += xxT
 
     def get_model_params(self):
-        # Return the parameter vector for each class (action)
-        # if self.mode == "torch":
-        #     return linear_layer.weight.data
-        if self.n_actions == 2 and (self.mode in ['sgd','logreg']):
-            return np.vstack([self.model.coef_, -1*self.model.coef_])
         return self.model.coef_
 
     def predict(self, X):
-        # X is num samples by num features
+        # picks action
         return self.model.predict(X)
 
-    def predict_proba(self, X):
-        return self.model.predict_proba(X)
-        
     def get_optimization_parameters(self):
-        """
-        Returns the parameters required for the convex optimization:
-        - theta_lr: Logistic regression parameters for each action.
-        - X_sum: the accumulated X^T X matrix (with regularization).
-        """
-        theta_lr = self.get_model_params()  # Get the logistic regression model's coefficients (theta)
-        #  X_sum, with regularization
-
-        X_sum = self.X_sum + np.eye(self.n_features) * self.lambda_
-        return theta_lr, X_sum
+        theta = self.get_model_params()  
+        return theta, self.X_sum, self.A_sum
 
     def get_ucb_lcb(self, context):
-        # this is valid when only considering logistic regression data
+        # TODO doesnt currently consider lr data
         context = context.reshape(-1)
         ucb = []
         lcb = []
-        theta, X_sum = self.get_optimization_parameters()
-        sigma = self.beta * np.sqrt(context.dot(inv(X_sum).dot(context)))
+        theta, X_sum, _ = self.get_optimization_parameters()
+        sigma = self.beta_log * np.sqrt(context.dot(inv(X_sum).dot(context)))
         for a in range(self.n_actions):
             ucb.append(theta[a].dot(context) + sigma)
             lcb.append(theta[a].dot(context) - sigma)
         return np.array(ucb), np.array(lcb)
+
+OnlineLogisticRegressionOracle = CombinedOnlineRegressionOracle
