dc84f6c3700ceab6b1c6a5c794532e617fbf182a
Rohan Banerjee <rohan.b.banerjee@gmail.com>
2025-04-06 17:48:22 -0400

    handle multi-seed in plotting

--- plot_tools.py
+++ plot_tools.py
@@ -21,7 +21,7 @@ def extract_from(filename):
         action_per_time = data['action_per_time']
     return reward_per_time, query_per_time, action_per_time
 
-def plot_mixucbs(Figure_dir='Figures', result_postfix="", result_root='', data_name=''):
+def plot_mixucbs(Figure_dir='Figures', result_postfix="", result_root='', data_name='', seeds=[42]):
     os.makedirs(Figure_dir, exist_ok=True)
     colors_list = sns.color_palette('colorblind', n_colors=6)
     modes = {'lin':['LinUCB'], 
@@ -37,118 +37,157 @@ def plot_mixucbs(Figure_dir='Figures', result_postfix="", result_root='', data_n
     for mode, marker in zip(modes.keys(), marker_list):
         modes[mode].append(marker)
     markerevery = 30
-    rewards = {}
-    queries = {}
-    actions = {}
+    rewards = {mode: {} for mode in modes.keys()}       # keying is mode/seed.
+    queries = {mode: {} for mode in modes.keys()}
+    actions = {mode: {} for mode in modes.keys()}
+    # data containers for each of the 3 subplots/metrics.
+    # (i.e. for each of the modes/algorithms, we have 3 signals of interest.)
+    cumulative_rewards = {mode: {} for mode in modes.keys()}       # keying is mode/seed.
+    cumulative_queries = {mode: {} for mode in modes.keys()}
+    avg_reward_noquery = {mode: {} for mode in modes.keys()}
 
     delta_values = None
 
-    for mode in modes.keys():
-        if mode in ['sq_oracle', 'lr_oracle']:
-            rewards[mode] = []
-            actions[mode] = []
-            dirname = os.path.join(result_root, f'{mode}_results{result_postfix}')
-            pkls = os.listdir(dirname)
-            for pklname in pkls:
-                reward_per_time, _, action_per_time = extract_from(os.path.join(dirname,pklname))
-                rewards[mode].append(reward_per_time)
-                actions[mode].append(action_per_time)
-        elif mode == 'lin':
-            rewards[mode] = []
-            queries[mode] = []
-            actions[mode] = []
-            dirname = os.path.join(result_root, f'{mode}_ucb_results{result_postfix}', '{}'.format(0))
-            pkls = os.listdir(dirname)
-            for pklname in pkls:
-                reward_per_time, query_per_time, action_per_time = extract_from(os.path.join(dirname,pklname))
-                rewards[mode].append(reward_per_time)
-                queries[mode].append(query_per_time)
-                actions[mode].append(action_per_time)
-        else:
-            rewards[mode] = {}
-            queries[mode] = {}
-            actions[mode] = {}
-            new_delta_values = np.sort(os.listdir(os.path.join(result_root, f'{mode}_ucb_results{result_postfix}')))
-            if delta_values is not None:
-                assert (delta_values == new_delta_values).all()
-            else:
-                delta_values = new_delta_values
-            for each_delta in delta_values:
-                print(each_delta)
-                rewards[mode][each_delta] = []
-                queries[mode][each_delta] = []
-                actions[mode][each_delta] = []
-                dirname = os.path.join(result_root, f'{mode}_ucb_results{result_postfix}', '{}'.format(each_delta))
+    ## Data gathering: loop across seeds.
+    ## Data containers (rewards, queries, actions) should contain seed as a key.
+
+    for seed in seeds:
+        # modes are algorithms.
+        for mode in modes.keys():
+            if mode in ['sq_oracle', 'lr_oracle']:
+                rewards[mode][seed] = []
+                actions[mode][seed] = []
+                dirname = os.path.join(result_root, f'seed_{seed:02d}', f'{mode}_results{result_postfix}')
+                pkls = os.listdir(dirname)
+                for pklname in pkls:
+                    reward_per_time, _, action_per_time = extract_from(os.path.join(dirname,pklname))
+                    rewards[mode][seed].append(reward_per_time)
+                    actions[mode][seed].append(action_per_time)
+            elif mode == 'lin':
+                rewards[mode][seed] = []
+                queries[mode][seed] = []
+                actions[mode][seed] = []
+                dirname = os.path.join(result_root, f'seed_{seed:02d}', f'{mode}_ucb_results{result_postfix}', '{}'.format(0))
                 pkls = os.listdir(dirname)
                 for pklname in pkls:
                     reward_per_time, query_per_time, action_per_time = extract_from(os.path.join(dirname,pklname))
-                    rewards[mode][each_delta].append(reward_per_time)
-                    queries[mode][each_delta].append(query_per_time)
-                    actions[mode][each_delta].append(action_per_time)
+                    rewards[mode][seed].append(reward_per_time)
+                    queries[mode][seed].append(query_per_time)
+                    actions[mode][seed].append(action_per_time)
+            else:
+                rewards[mode][seed] = {}
+                queries[mode][seed] = {}
+                actions[mode][seed] = {}
+                new_delta_values = np.sort(os.listdir(os.path.join(result_root, f'seed_{seed:02d}', f'{mode}_ucb_results{result_postfix}')))
+                if delta_values is not None:
+                    assert (delta_values == new_delta_values).all()
+                else:
+                    delta_values = new_delta_values
+                for each_delta in delta_values:
+                    print(each_delta)
+                    rewards[mode][seed][each_delta] = []
+                    queries[mode][seed][each_delta] = []
+                    actions[mode][seed][each_delta] = []
+                    dirname = os.path.join(result_root, f'seed_{seed:02d}', f'{mode}_ucb_results{result_postfix}', '{}'.format(each_delta))
+                    pkls = os.listdir(dirname)
+                    for pklname in pkls:
+                        reward_per_time, query_per_time, action_per_time = extract_from(os.path.join(dirname,pklname))
+                        rewards[mode][seed][each_delta].append(reward_per_time)
+                        queries[mode][seed][each_delta].append(query_per_time)
+                        actions[mode][seed][each_delta].append(action_per_time)
 
-    cumulative_rewards = {}
-    cumulative_queries = {}
-    avg_reward_noquery = {}
-    def accum(list_of_lists):
-        return np.array([np.cumsum(l) for l in list_of_lists])
-    def masked_avg(list_of_values, list_of_masks):
-        ret = []
-        for vals, mask in zip(list_of_values, list_of_masks):
-            mask = (1-np.array(mask)).astype(int)
+        def accum(list_of_lists):
+            return np.array([np.cumsum(l) for l in list_of_lists])
+        def masked_avg(list_of_values, list_of_masks):
+            ret = []
+            for vals, mask in zip(list_of_values, list_of_masks):
+                mask = (1-np.array(mask)).astype(int)
 
-            temp = np.zeros_like(vals)
-            for i in range(len(temp)):
-                if mask[i]:
-                    temp[i] = vals[i]
-            ret.append(np.cumsum(temp) / np.cumsum(mask))
+                temp = np.zeros_like(vals)
+                for i in range(len(temp)):
+                    if mask[i]:
+                        temp[i] = vals[i]
+                ret.append(np.cumsum(temp) / np.cumsum(mask))
 
-        return np.array(ret)
-    # cumulative reward over time
-    for mode in modes.keys():
-        if mode in ['sq_oracle', 'lr_oracle']:
-            cumulative_rewards[mode] = np.mean(accum(rewards[mode]), axis=0)
-            avg_reward_noquery[mode] = np.mean(masked_avg(rewards[mode],np.zeros_like(rewards[mode])), axis=0)
-        elif mode == 'lin':
-            cumulative_rewards[mode] = np.mean(accum(rewards[mode]), axis=0)
-            cumulative_queries[mode] = np.mean(accum(queries[mode]), axis=0)
-            avg_reward_noquery[mode] = np.mean(masked_avg(rewards[mode], queries[mode]), axis=0)
-        else:
-            cumulative_rewards[mode] = {}
-            cumulative_queries[mode] = {}
-            avg_reward_noquery[mode] = {}
-            for each_delta in delta_values:
-                cumulative_rewards[mode][each_delta] = np.mean(accum(rewards[mode][each_delta]), axis=0)
-                cumulative_queries[mode][each_delta] = np.mean(accum(queries[mode][each_delta]), axis=0)
-                avg_reward_noquery[mode][each_delta] = np.mean(masked_avg(rewards[mode][each_delta], queries[mode][each_delta]), axis=0)
+            return np.array(ret)
+        # cumulative reward over time
+        for mode in modes.keys():
+            if mode in ['sq_oracle', 'lr_oracle']:
+                cumulative_rewards[mode][seed] = np.mean(accum(rewards[mode][seed]), axis=0)
+                avg_reward_noquery[mode][seed] = np.mean(masked_avg(rewards[mode][seed],np.zeros_like(rewards[mode][seed])), axis=0)
+            elif mode == 'lin':
+                cumulative_rewards[mode][seed] = np.mean(accum(rewards[mode][seed]), axis=0)
+                cumulative_queries[mode][seed] = np.mean(accum(queries[mode][seed]), axis=0)
+                avg_reward_noquery[mode][seed] = np.mean(masked_avg(rewards[mode][seed], queries[mode][seed]), axis=0)
+            else:
+                cumulative_rewards[mode][seed] = {}
+                cumulative_queries[mode][seed] = {}
+                avg_reward_noquery[mode][seed] = {}
+                for each_delta in delta_values:
+                    cumulative_rewards[mode][seed][each_delta] = np.mean(accum(rewards[mode][seed][each_delta]), axis=0)
+                    cumulative_queries[mode][seed][each_delta] = np.mean(accum(queries[mode][seed][each_delta]), axis=0)
+                    avg_reward_noquery[mode][seed][each_delta] = np.mean(masked_avg(rewards[mode][seed][each_delta], queries[mode][seed][each_delta]), axis=0)
 
+    print(f"Seeds: {seeds}")
+
+    # plot the data.
+    ## [meaning, for each mode/algorithm pair, plot the median and the quartiles (or mean and std) over the seeds.]
     fig_list, axs_list = [], []
     for i, each_delta in enumerate(delta_values):
         fig, axs = plt.subplots(1, len(delta_values), figsize=(10, 3))
         fig_list.append(fig)
         axs_list.append(axs)
     
+    # Plot 1: Cumulative Reward
     for i, each_delta in enumerate(delta_values):
         ax = axs_list[i][0]
         for mode in ['sq_oracle', 'lr_oracle', 'lin']:
-            ax.plot(cumulative_rewards[mode], label=modes[mode][0], color=modes[mode][1], marker=modes[mode][2], markevery=markerevery)
+            # Calculate mean + std of cumulative_rewards over the seeds.
+            mean_cumulative_rewards = np.mean([cumulative_rewards[mode][seed] for seed in seeds], axis=0)
+            std_cumulative_rewards = np.std([cumulative_rewards[mode][seed] for seed in seeds], axis=0)
+            ax.plot(mean_cumulative_rewards, label=modes[mode][0], color=modes[mode][1], marker=modes[mode][2], markevery=markerevery)
+            ax.fill_between(range(len(mean_cumulative_rewards)), mean_cumulative_rewards - std_cumulative_rewards, mean_cumulative_rewards + std_cumulative_rewards, \
+                            color=modes[mode][1], alpha=0.2)
         for mode in ['mixI','mixII', 'mixIII']:
-            ax.plot(cumulative_rewards[mode][each_delta], label=modes[mode][0], color=modes[mode][1], marker=modes[mode][2], markevery=markerevery)
+            # Calculate mean + std of cumulative_rewards over the seeds.
+            mean_cumulative_rewards = np.mean([cumulative_rewards[mode][seed][each_delta] for seed in seeds], axis=0)
+            std_cumulative_rewards = np.std([cumulative_rewards[mode][seed][each_delta] for seed in seeds], axis=0)
+            ax.plot(mean_cumulative_rewards, label=modes[mode][0], color=modes[mode][1], marker=modes[mode][2], markevery=markerevery)
+            ax.fill_between(range(len(mean_cumulative_rewards)), mean_cumulative_rewards - std_cumulative_rewards, mean_cumulative_rewards + std_cumulative_rewards, \
+                            color=modes[mode][1], alpha=0.2)
         ax.set_title('Cumulative Reward ($\Delta={}$)'.format(each_delta))
         ax.set_xlabel('time steps')
 
+    # Plot 2: Cumulative Queries
     for i, each_delta in enumerate(delta_values):
         ax = axs_list[i][2]
         for mode in ['mixI','mixII', 'mixIII']:
-            ax.plot(cumulative_queries[mode][each_delta], label=modes[mode][0], color=modes[mode][1], marker=modes[mode][2], markevery=markerevery)
+            # Calculate mean + std of cumulative_queries over the seeds.
+            mean_cumulative_queries = np.mean([cumulative_queries[mode][seed][each_delta] for seed in seeds], axis=0)
+            std_cumulative_queries = np.std([cumulative_queries[mode][seed][each_delta] for seed in seeds], axis=0)
+            ax.plot(mean_cumulative_queries, label=modes[mode][0], color=modes[mode][1], marker=modes[mode][2], markevery=markerevery)
+            ax.fill_between(range(len(mean_cumulative_queries)), mean_cumulative_queries - std_cumulative_queries, mean_cumulative_queries + std_cumulative_queries, \
+                            color=modes[mode][1], alpha=0.2)
         ax.set_title('Cumulative Queries ($\Delta={}$)'.format(each_delta))
         ax.set_xlabel('time steps')
 
+    # Plot 3: Average Autonomous Reward
     for i, each_delta in enumerate(delta_values):
         ax = axs_list[i][1]
         for mode in ['sq_oracle', 'lr_oracle', 'lin']:
-            ax.plot(avg_reward_noquery[mode], label=modes[mode][0], color=modes[mode][1], marker=modes[mode][2], markevery=markerevery)
+            # Calculate mean + std of avg_reward_noquery over the seeds.
+            mean_avg_reward_noquery = np.mean([avg_reward_noquery[mode][seed] for seed in seeds], axis=0)
+            std_avg_reward_noquery = np.std([avg_reward_noquery[mode][seed] for seed in seeds], axis=0)
+            ax.plot(mean_avg_reward_noquery, label=modes[mode][0], color=modes[mode][1], marker=modes[mode][2], markevery=markerevery)
+            ax.fill_between(range(len(mean_avg_reward_noquery)), mean_avg_reward_noquery - std_avg_reward_noquery, mean_avg_reward_noquery + std_avg_reward_noquery, \
+                            color=modes[mode][1], alpha=0.2)
         for mode in ['mixI','mixII', 'mixIII']:
-            ax.plot(avg_reward_noquery[mode][each_delta], label=modes[mode][0], color=modes[mode][1], marker=modes[mode][2], markevery=markerevery)
+            # Calculate mean + std of avg_reward_noquery over the seeds.
+            mean_avg_reward_noquery = np.mean([avg_reward_noquery[mode][seed][each_delta] for seed in seeds], axis=0)
+            std_avg_reward_noquery = np.std([avg_reward_noquery[mode][seed][each_delta] for seed in seeds], axis=0)
+            ax.plot(mean_avg_reward_noquery, label=modes[mode][0], color=modes[mode][1], marker=modes[mode][2], markevery=markerevery)
+            ax.fill_between(range(len(mean_avg_reward_noquery)), mean_avg_reward_noquery - std_avg_reward_noquery, mean_avg_reward_noquery + std_avg_reward_noquery, \
+                            color=modes[mode][1], alpha=0.2)
         ax.set_title('Average Autonomous Reward ($\Delta={}$)'.format(each_delta))
         ax.set_xlabel('time steps')
 
@@ -165,10 +204,12 @@ def plot_mixucbs(Figure_dir='Figures', result_postfix="", result_root='', data_n
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Script for processing data.")
     parser.add_argument("--data_name", type=str, default='', help="Name of the dataset to use.")
+    parser.add_argument("--seeds", nargs='+', type=int, default=[42], help="List of seeds to use.")
     args = parser.parse_args()
     data_name = args.data_name
 
     result_root = data_name
     Figure_dir = f'Figures/{result_root}'
+    seeds= args.seeds
     os.makedirs(Figure_dir, exist_ok=True)
-    plot_mixucbs(Figure_dir=Figure_dir, result_root=result_root, data_name=data_name)
+    plot_mixucbs(Figure_dir=Figure_dir, result_root=result_root, data_name=data_name, seeds=seeds)
--- run_all.sh
+++ run_all.sh
@@ -1,47 +1,69 @@
-# (1) Data generation/hyperparameter settings
+# (0) Random seeds
 
 SEEDS='42 0 1 2 3'
 
-## for classification datasets
-DELTA='4 5 6'
-# data_name=MedNIST  # heart_disease
-data_name=heart_disease
-for seed in ${SEEDS}; do
-    python generate_multilabel_data.py --data_name ${data_name} --seed ${seed}
-done
-# PICKLE_FILE="multilabel_data_${data_name}_42.pkl"
+# (1) Data generation/hyperparameter settings
 
-## for rohan's synthetic data
-DELTA='2 3 4'
-data_name=synthetic
-noise_std='0.2'
-for seed in ${SEEDS}; do
-    python generate_multilabel_data.py --data_name ${data_name} --seed ${seed} --noise_std ${noise_std}
-done
+# ## for classification datasets
+# DELTA='4 5 6'
+# data_names='MedNIST heart_disease'
+# for data_name in ${data_names}; do
+#     for seed in ${SEEDS}; do
+#         python generate_multilabel_data.py --data_name ${data_name} --seed ${seed}
+#     done
+# done
 
-## for spanet data.
-DELTA='3 4 5'
-data_name=spanet
-pca_dim='5'
-for seed in ${SEEDS}; do
-    python generate_multilabel_data.py --data_name ${data_name} --seed ${seed} --n_features ${pca_dim}
-done
+# ## for rohan's synthetic data
+# DELTA='2 3 4'
+# data_name=synthetic
+# noise_std='0.2'
+# for seed in ${SEEDS}; do
+#     python generate_multilabel_data.py --data_name ${data_name} --seed ${seed} --noise_std ${noise_std}
+# done
+
+# ## for spanet data.
+# DELTA='3 4 5'
+# data_name=spanet
+# pca_dim='5'
+# for seed in ${SEEDS}; do
+#     python generate_multilabel_data.py --data_name ${data_name} --seed ${seed} --n_features ${pca_dim}
+# done
 
 TEMP=1
 BETA=10
 ALPHA=5
 
 # (2) Algorithm execution
+# Loop through all datasets and all seeds.
 
-# python run_allucb.py --T 300 --mode lin --pickle_file ${PICKLE_FILE} --alpha ${ALPHA} --data_name ${data_name}
-# python run_allucb.py --T 300 --mode mixI --pickle_file ${PICKLE_FILE} --beta ${BETA} --alpha ${ALPHA} --data_name ${data_name} --delta ${DELTA}
-# python run_allucb.py --T 300 --mode mixII --pickle_file ${PICKLE_FILE} --beta ${BETA} --alpha ${ALPHA} --data_name ${data_name} --delta ${DELTA}
-# python run_allucb.py --T 300 --mode mixIII --pickle_file ${PICKLE_FILE} --alpha ${ALPHA} --data_name ${data_name} --delta ${DELTA}
-
-# python run_allucb.py --T 300 --mode sq_oracle --pickle_file ${PICKLE_FILE} --data_name ${data_name}
-# python run_allucb.py --T 300 --mode lr_oracle --pickle_file ${PICKLE_FILE} --data_name ${data_name}
+data_names_all='MedNIST heart_disease synthetic spanet'
+# for data_name in ${data_names_all}; do
+#     if [ ${data_name} == 'MedNIST' ]; then
+#         DELTA='4 5 6'
+#     elif [ ${data_name} == 'heart_disease' ]; then
+#         DELTA='4 5 6'
+#     elif [ ${data_name} == 'synthetic' ]; then
+#         DELTA='2 3 4'
+#     elif [ ${data_name} == 'spanet' ]; then
+#         DELTA='3 4 5'
+#     fi
+#     for seed in ${SEEDS}; do
+#         formatted_seed=$(printf "%02d" $seed)
+#         PICKLE_FILE="raw_data/multilabel_data_${data_name}_${formatted_seed}.pkl"        
+#         python run_allucb.py --T 300 --mode lin --pickle_file ${PICKLE_FILE} --alpha ${ALPHA} --data_name ${data_name} --seed ${seed}
+#         python run_allucb.py --T 300 --mode mixI --pickle_file ${PICKLE_FILE} --beta ${BETA} --alpha ${ALPHA} --data_name ${data_name} --delta ${DELTA} --seed ${seed}
+#         python run_allucb.py --T 300 --mode mixII --pickle_file ${PICKLE_FILE} --beta ${BETA} --alpha ${ALPHA} --data_name ${data_name} --delta ${DELTA} --seed ${seed}
+#         python run_allucb.py --T 300 --mode mixIII --pickle_file ${PICKLE_FILE} --alpha ${ALPHA} --data_name ${data_name} --delta ${DELTA} --seed ${seed}
+#         python run_allucb.py --T 300 --mode sq_oracle --pickle_file ${PICKLE_FILE} --data_name ${data_name} --seed ${seed}
+#         python run_allucb.py --T 300 --mode lr_oracle --pickle_file ${PICKLE_FILE} --data_name ${data_name} --seed ${seed}
+#     done
+# done
 
 # (3) Plotting
 
 # ROHAN: assumes that the {data_name} directory exists.
-# python plot_tools.py --data_name ${data_name}
+for data_name in ${data_names_all}; do
+    python plot_tools.py --data_name ${data_name} --seeds ${SEEDS}
+done
+# data_name=synthetic
+# python plot_tools.py --data_name ${data_name} --seeds ${SEEDS}
