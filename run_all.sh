7f7b58a3167e186600356ece893f1a4914c30a38
Rohan Banerjee <rohan.b.banerjee@gmail.com>
2025-06-23 17:50:38 -0400

    update overall bash script after delta scalings

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,16 @@
+#!/bin/bash
+## GENERAL SCRIPT STRUCTURE
+# 0. Random seeds
+# 1. Data generation/hyperparameter settings
+# 2. Algorithm execution
+# 3. Plotting
+####################################
+
 # (0) Random seeds
 
 SEEDS='42 0 1 2 3'
+# SEEDS='42'
+# SEEDS='3'
 
 # (1) Data generation/hyperparameter settings
 
@@ -29,35 +39,62 @@ SEEDS='42 0 1 2 3'
 #     python generate_multilabel_data.py --data_name ${data_name} --seed ${seed} --n_features ${pca_dim}
 # done
 
-TEMP=1
-BETA=10
-ALPHA=5
+# TEMP=1              # NOTE: not used.
+# ALPHA=5
+# BETA=10
 
 # (2) Algorithm execution
 # Loop through all datasets and all seeds.
 
 data_names_all='MedNIST heart_disease synthetic spanet'
-# for data_name in ${data_names_all}; do
-#     if [ ${data_name} == 'MedNIST' ]; then
-#         DELTA='4 5 6'
-#     elif [ ${data_name} == 'heart_disease' ]; then
-#         DELTA='4 5 6'
-#     elif [ ${data_name} == 'synthetic' ]; then
-#         DELTA='2 3 4'
-#     elif [ ${data_name} == 'spanet' ]; then
-#         DELTA='3 4 5'
-#     fi
-#     for seed in ${SEEDS}; do
-#         formatted_seed=$(printf "%02d" $seed)
-#         PICKLE_FILE="raw_data/multilabel_data_${data_name}_${formatted_seed}.pkl"        
-#         python run_allucb.py --T 300 --mode lin --pickle_file ${PICKLE_FILE} --alpha ${ALPHA} --data_name ${data_name} --seed ${seed}
-#         python run_allucb.py --T 300 --mode mixI --pickle_file ${PICKLE_FILE} --beta ${BETA} --alpha ${ALPHA} --data_name ${data_name} --delta ${DELTA} --seed ${seed}
-#         python run_allucb.py --T 300 --mode mixII --pickle_file ${PICKLE_FILE} --beta ${BETA} --alpha ${ALPHA} --data_name ${data_name} --delta ${DELTA} --seed ${seed}
-#         python run_allucb.py --T 300 --mode mixIII --pickle_file ${PICKLE_FILE} --alpha ${ALPHA} --data_name ${data_name} --delta ${DELTA} --seed ${seed}
-#         python run_allucb.py --T 300 --mode sq_oracle --pickle_file ${PICKLE_FILE} --data_name ${data_name} --seed ${seed}
-#         python run_allucb.py --T 300 --mode lr_oracle --pickle_file ${PICKLE_FILE} --data_name ${data_name} --seed ${seed}
-#     done
-# done
+# data_names_all='synthetic'
+for data_name in ${data_names_all}; do
+    if [ ${data_name} == 'MedNIST' ]; then
+        # DELTA='4 5 6'
+        # ALPHA='5'             # alpha_orig
+        # BETA='10'              # beta_orig
+        DELTA='0.5 0.625 0.75'
+        ALPHA='0.625'
+        BETA='1.25'
+        LAMBDA='0.001'          # ORIGINAL LAMBDA.    
+    elif [ ${data_name} == 'heart_disease' ]; then
+        # DELTA='4 5 6'
+        # ALPHA='5'             # alpha_orig
+        # BETA='10'              # beta_orig
+        DELTA='0.5 0.625 0.75'
+        ALPHA='0.625'
+        BETA='1.25'
+        LAMBDA='0.001'          # ORIGINAL LAMBDA.    
+    elif [ ${data_name} == 'synthetic' ]; then
+        # DELTA='2 3 4'
+        # ALPHA='5'             # alpha_orig
+        # BETA='10'              # beta_orig
+        DELTA='0.5 0.75 1.0'    # Let C=4. delta_orig/C
+        ALPHA='1.25'             # alpha_orig/C
+        BETA='2.5'              # beta_orig/C
+        LAMBDA='0.001'          # ORIGINAL LAMBDA.    
+    elif [ ${data_name} == 'spanet' ]; then
+        DELTA='3 4 5'
+        ALPHA='5'             # alpha_orig
+        BETA='10'              # beta_orig
+        DELTA='0.5 0.625 0.75'
+        ALPHA='0.625'
+        BETA='1.25'
+        LAMBDA='0.001'          # ORIGINAL LAMBDA.    
+    fi
+    for seed in ${SEEDS}; do
+        formatted_seed=$(printf "%02d" $seed)
+        PICKLE_FILE="raw_data/multilabel_data_${data_name}_${formatted_seed}.pkl"        
+        python run_allucb.py --T 300 --mode lin --pickle_file ${PICKLE_FILE} --alpha ${ALPHA} --data_name ${data_name} --seed ${seed} --lambda_ ${LAMBDA}
+        python run_allucb.py --T 300 --mode mixI --pickle_file ${PICKLE_FILE} --beta ${BETA} --alpha ${ALPHA} --data_name ${data_name} --delta ${DELTA} --seed ${seed} --lambda_ ${LAMBDA}
+        python run_allucb.py --T 300 --mode mixII --pickle_file ${PICKLE_FILE} --beta ${BETA} --alpha ${ALPHA} --data_name ${data_name} --delta ${DELTA} --seed ${seed} --lambda_ ${LAMBDA}
+        python run_allucb.py --T 300 --mode mixIII --pickle_file ${PICKLE_FILE} --alpha ${ALPHA} --data_name ${data_name} --delta ${DELTA} --seed ${seed} --lambda_ ${LAMBDA}
+        python run_allucb.py --T 300 --mode sq_oracle --pickle_file ${PICKLE_FILE} --data_name ${data_name} --seed ${seed} --lambda_ ${LAMBDA}
+        python run_allucb.py --T 300 --mode lr_oracle --pickle_file ${PICKLE_FILE} --data_name ${data_name} --seed ${seed} --lambda_ ${LAMBDA}
+        python run_allucb.py --T 300 --mode perfect_exp --pickle_file ${PICKLE_FILE} --data_name ${data_name} --seed ${seed} --lambda_ ${LAMBDA}
+        python run_allucb.py --T 300 --mode noisy_exp --pickle_file ${PICKLE_FILE} --data_name ${data_name} --seed ${seed} --lambda_ ${LAMBDA}
+    done
+done
 
 # (3) Plotting
 
